17824c6a70a88d510aa492fd69ba7fa6d1a85424
fszhuangb <fszhuangb@gmail.com>
2020-06-16 16:08:37 +0800

    add new exercise

--- ch1/exercise6_server.c
+++ ch1/exercise6_server.c
@@ -5,62 +5,44 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-void error_handling(char* message);
-
 int main(int argc, char* argv[])
 {
-    int serv_sock;
-    int clnt_sock;
+    int serv_sock;  // server socket
+    int clnt_sock;  // client socket
 
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
+    struct sockaddr_in serv_addr; // server address
+    struct sockaddr_in clnt_addr; // client address
     socklen_t clnt_addr_size;
 
-    char message[] = "Hello World!";
+    char message1[] = "Hello";
+    char message2[] = "World";
 
-    if (argc != 2)
-    {
-        printf("Usage : %s <port>\n", argv[0]);
-        exit(1);
-    }
-
-// call the socket function to create socket
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (serv_sock == -1)
-        error_handling("socket() error");
 
-    // set the server addr to 0
     memset(&serv_addr, 0, sizeof(serv_addr));
 
-    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    // endia conversions
-    // htons(host to network) short data
     serv_addr.sin_port = htons(atoi(argv[1]));
 
-// bind function to allocate address and port
+    // bind function to allocate address and port
     if (bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
-        error_handling("bing() error");
+        return -1;
 // use the listen turning the socket to accept
     if (listen(serv_sock, 5) == -1)
-        error_handling("listen() error"); 
-    
+        return -1; 
+
     clnt_addr_size = sizeof(clnt_addr);
 // use the accept function to handle the connection request 
     clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
     if(clnt_sock == -1)
         error_handling("accept() error");
 
-    write(clnt_sock, message, sizeof(message));
+    write(clnt_sock, message1, sizeof(message1));
+    write(clnt_sock, message2, sizeof(message2));
     close(clnt_sock);
     close(serv_sock);
 
     return 0;
-}
 
-void error_handling(char* message)
-{
-    fputs(message, stderr);
-    fputc('\n', stderr);
-    exit(1);
 }
